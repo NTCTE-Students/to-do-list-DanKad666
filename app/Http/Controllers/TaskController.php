2272fecfc0d03042775500e2bbaa8e80dd1cfd29
kadaev danil <kadaev.danil@nttek.ru>
2024-06-07 18:36:21 +0500

    pr14

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+public function index()
+{
+$tasks = Task::all();
+return view('tasks.index', compact('tasks'));
+}
+
+public function create()
+{
+return view('tasks.create');
+}
+
+public function store(Request $request)
+{
+Task::create($request->all());
+return redirect('/');
+}
+
+public function edit(Task $task)
+{
+return view('tasks.edit', compact('task'));
+}
+
+public function update(Request $request, Task $task)
+{
+$task->update($request->all());
+return redirect('/');
+}
+
+public function destroy(Task $task)
+{
+$task->delete();
+return redirect('/');
+}
+
+public function complete(Task $task)
+{
+$task->update(['completed' => true]);
+return redirect('/');
+}
+}
